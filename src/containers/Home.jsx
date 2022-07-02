2990fa42a587b3348bad1f383973cf51feec3d45
Luiz Bonini <lc.bonini13@gmail.com>
2022-07-01 21:37:55 -0300

    Feat: Primeiro commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../public/imgs/logos/color logo.svg'
+
+const home = () => {
+    return ( 
+        <header>
+            <img src={logo} alt="" />
+        </header>
+     );
+}
+ 
+export default home;
